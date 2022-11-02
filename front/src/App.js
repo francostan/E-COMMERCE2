a3b34c867e3a0b9302dd081229dd972cd36062d2
Lioxcas <licastrox@gmail.com>
2022-11-02 13:04:39 -0300

    agregando h2

--- front/src/App.js
+++ front/src/App.js
@@ -9,7 +9,8 @@ const App = () => {
       <p>¡Bienvenidos a E-COMMERCE!</p>
       <Routes>
         <Route path="/" element={<p />} />
-        <Route path="/" element={<h1/>} />
+        <Route path="/" element={<h1 />} />
+        <Route path="/" element={<h2 />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
