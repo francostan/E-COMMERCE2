0b82c7b17ebe7aed089f0315910b0586ea14bc37
Lioxcas <licastrox@gmail.com>
2022-11-03 18:14:08 -0300

    Add Register Front

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,17 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleEmail = (e) => {
-    e.preventDefault();
-    setEmail(e.target.value);
-  };
-  const handlePassword = (e) => {
-    e.preventDefault();
-    setPassword(e.target.value);
-  };
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log("Hola");
+    const data = { email, password };
+    axios
+      .post("/api/users/login", data)
+      .then(() => console.log("loggin succesful"))
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -25,17 +22,20 @@ const Login = () => {
 
       <form>
         <div>
-          <label for="email">Email</label>
-          <input type="text" onChange={handleEmail} />
+          <label htmlfor="email">Email: </label>
+          <input type="text" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
-          <label for="password">Password</label>
-          <input type="password" onChange={handlePassword} />
+          <label htmlfor="password">Password: </label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <div>
             <input type="checkbox" />
-            <label for="password">Recuerdame</label>
+            <label htmlfor="password">Recuerdame</label>
           </div>
           <button type="submit" onClick={handleClick}>
             Ingresa
