33e3b2c875428f87e44ad69d3a70ab7450b95088
Rodriix99 <roescal347@gmail.com>
2022-11-08 12:34:14 -0300

    SideBar terminada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Header from "../commons/Header";
 
+import Header from "../commons/Header";
+import SideBar from "./SideBar";
+import styles from "../Styles/Home.module.css";
 function Home() {
   return (
     <div>
-      <Header />
+      <SideBar />
+      <div className={styles.space}></div>
+      <Header className={styles.header} />
     </div>
   );
 }
