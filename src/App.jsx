87db34e81a09f546d09eace36f9fe1183f262a62
Lioxcas <licastrox@gmail.com>
2022-11-09 17:33:26 -0300

    App resolved conflic

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import Logout from "./components/Logout";
 import Navbar from "./components/NavBar";
 import Home from "./components/Home";
 import Favoritos from "./components/Favoritos";
 import { Navigate } from "react-router-dom";
 import "./Styles/Global.css";
-import Carrito from "./components/Carrito";
-import ProductDetails from "./components/ProductDetails";
-
+import "./Styles/reset.css";
+import ProductDetails from "../src/components/ProductDetails";
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "./store/user";
 //Consultar a fran el dispatch del user, para el ternario de las rutas cuando exista user
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get("/api/users/me")
+      .then((res) => res.data)
+      .then((user) => {
+        dispatch(setUser(user));
+      })
+      .catch(() =>
+        console.error("Necesitas loguearte para obtener tu usuario.")
+      );
+  }, []);
+  const user = useSelector((state) => state.user);
+
   return (
     <div>
       <Navbar />
       <Routes>
+        <Route path="/*" element={<Home />} />
         <Route path="/" element={<Navigate to={"/home"} />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/favoritos" element={<Favoritos />} />
+        {!user.id ? (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        ) : (
+          <>
+            <Route path={`/favoritos`} element={<Favoritos />} />
+          </>
+        )}
+
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/cart/:id" element={<Carrito />} />
       </Routes>
     </div>
   );
