cf4e5f40b85ffe5e2d5249267b8c927131422137
Rodriix99 <roescal347@gmail.com>
2022-11-09 11:46:43 -0300

    Metodos de clase e instancia borrados

--- api/models/Favoritos.js
+++ api/models/Favoritos.js
@@ -1,16 +1,7 @@
 const S = require("sequelize");
 const db = require("../config/db");
 
-class Favoritos extends S.Model {
-  hasFav(userId, productId) {
-    console.log(this);
-    this.userId && this.productId ? true : false;
-  }
-
-  static tieneFavoritos(userId, productId) {
-    return Favoritos.findOne({ where: { userId, productId } });
-  }
-}
+class Favoritos extends S.Model {}
 
 Favoritos.init({}, { sequelize: db, modelName: "favoritos" });
 
