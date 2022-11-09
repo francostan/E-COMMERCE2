d08206c8b2a54c6be04fed67f475a95b460197ef
Rodriix99 <roescal347@gmail.com>
2022-11-09 19:19:29 -0300

    listro

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,9 +4,11 @@ const userRouter = require("./userRouter");
 const productsRouter = require("./productsRouter");
 const cartRouter = require("./cartRouter");
 const favoritosRouter = require("./favoritosRouter");
+const payRouter = require("./payRouter");
 //centralizamos las rutas de products y users
 router.use("/users", userRouter);
 router.use("/products", productsRouter);
 router.use("/carts", cartRouter);
 router.use("/favoritos", favoritosRouter);
+router.use("/pay", payRouter);
 module.exports = router;
